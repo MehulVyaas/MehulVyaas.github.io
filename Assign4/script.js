55b671ba4b60d071dc4a1e993a96b0b6860648df
MehulVyaas <102595612+MehulVyaas@users.noreply.github.com>
2022-03-29 18:30:51 +0530

    Update script.js

--- Assign4/script.js
+++ Assign4/script.js
@@ -1,14 +1,14 @@
 var names=new Array();
-names[0]="Yaakov";
-names[1]="John";
-names[2]="Jen";
-names[3]="jason";
-names[4]="paul";
-names[5]="frank";
-names[6]="larry";
-names[7]="paula";
-names[8]="laura";
-names[9]="jim";
+names[0]="David";
+names[1]="Max";
+names[2]="Charlotte";
+names[3]="Micheal";
+names[4]="Johnathan";
+names[5]="Albert";
+names[6]="Noel";
+names[7]="Yuliya";
+names[8]="Hunter";
+names[9]="Carter";
 
 
 for (var i = 0; i < names.length; i++) {
